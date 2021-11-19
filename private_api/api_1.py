07cf04bf04b4a866a63542f6a534489a74fe917a
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-11-19 14:55:49 -0500

    email commit

--- private_api/api_1.py
+++ private_api/api_1.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # Class for Mysql database connection.
 import mysql_utility
+import smtplib
 
 # FastAPI instantiation.
 app = FastAPI()
@@ -21,12 +22,45 @@ app.add_middleware(
 )
 
 
-@app.post('/private/send_mail')
+def listToStringWithoutBrackets(list1):
+    return str(list1).replace('(', '').replace(')', '').replace(',', '').replace('[', '').replace(']', '').replace('{',
+                                                                                                                   '').replace(
+        '}', '')
+
+
+@app.post('/api/private/send_mail')
 async def send_mail(cedula: str, motivo: str, monto: float):
-    query_return = db_connection.execute_query(db_connection.sql_dict.get('login'),
+    print('cedula:', cedula, ' <---')
+    query_return = db_connection.execute_query(db_connection.sql_dict.get('buscar_usuario'),
                                                (cedula,))
+    print(len(query_return))
     match len(query_return):
         case 0:
             raise HTTPException(status_code=500, detail='No existe el usuario con la cedula ingresada')
         case _:
-            return ''
+            # correo = listToStringWithoutBrackets(query_return)
+            correo = query_return[0][0]
+            print(correo)
+            send_email(correo, motivo, monto)
+            return {'status': 'successful'}
+
+
+def send_email(correo: str, descripcion: str, valor: float):
+    gmail_user = 'alvaradolayonardo@gmail.com'
+    gmail_password = 'mqjjqbpmctkleorb'
+
+    sent_from = gmail_user
+    to = [correo]
+    body = f"""{descripcion}\n
+    {valor}"""
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, body)
+        server.close()
+        print('Email sent!')
+    except Exception as e:
+        print(e)
+        pass
